325203a56a78630ebf02f7dbcb496f8efc3fad56
Kimia Montazeri <kimia.montazeri@yahoo.com>
2022-01-05 14:57:34 +0330

    added the script

---
+++ basics/projects/todo-list/script.js
@@ -0,0 +1,106 @@
+const listsContainer       = document.querySelector('[data-lists]')
+const newListForm          = document.querySelector('[data-new-list-form]')
+const newListInput         = document.querySelector('[data-new-list-input]')
+const deleteListButton     = document.querySelector('[data-delete-list-button]')
+const listDisplayContainer = document.querySelector('[data-list-display-container]')
+const listTitleElement     = document.querySelector('[data-list-title]')
+const listCountElement     = document.querySelector('[data-list-count]')
+const tasksContainer       = document.querySelector('[data-tasks]')
+const taskTemplate         = document.getElementById('task-template')
+
+// store the user's data in the browser (local storage)
+// making a namespace prevents you from overwriting the info that is already in the storage, or preventing other websites from overwriting this local storage
+const LOCAL_STORAGE_LIST_KEY = 'task.lists' 
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+newListForm.addEventListener('submit', e => {
+    // stops the page from submitting 
+    e.preventDefault()
+    const listName = newListInput.value
+    if (listName == null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null
+    lists.push(list)
+    saveAndRender()
+})
+
+// adds a click event listener for li element inside the div
+listsContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId
+        saveAndRender()
+    }
+})
+
+deleteListButton.addEventListener('click', e => {
+    lists = lists.filter(list => list.id !== selectedListId)
+    selectedListId = null
+    saveAndRender()
+})
+
+function createList(name) {
+    return { id: Date.now().toString(), name: name, tasks: [{
+        id: 1,
+        name: 'wefef',
+        complete: false
+    }] }
+}
+
+function saveAndRender() {
+    save()
+    render()
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
+}
+
+function render() {
+    clearElement(listsContainer)
+    renderList()
+    const selectedList = lists.find(list => list.id === selectedListId)
+    if (selectedListId == null) {
+        listDisplayContainer.style.display = 'none'
+    } else {
+        listDisplayContainer.style.display = '' // revert it back to what is was 
+        listTitleElement.innerText = selectedList.name
+        renderTaskCount(selectedList)
+        clearElement(tasksContainer)
+        renderTasks(selectedList)
+    }
+}
+
+function renderList() {
+    lists.forEach(list => {
+        const listElement = document.createElement('li')
+        listElement.classList.add("list-name")
+        listElement.dataset.listId = list.id
+        listElement.innerText = list.name
+        if (list.id === selectedListId) {
+            listElement.classList.add('active-list')
+        }
+        listsContainer.appendChild(listElement)
+    })
+}
+
+function renderTasks(selectedList) {
+    selectedList.tasks.forEach(task => {
+    })
+}
+
+function renderTaskCount(selectedList) {
+    const incompleteTasksCount = selectedList.tasks.filter(task => !task.complete).length
+    const taskString = incompleteTasksCount === 1 ? "task" : "tasks"
+    listCountElement.innerText = `${incompleteTasksCount} ${taskString} remaining`
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+render()
