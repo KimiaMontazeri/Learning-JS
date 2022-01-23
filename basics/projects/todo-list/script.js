ef7da00c570fbbc3d561b844fcb14718c770edeb
Kimia Montazeri <kimia.montazeri@yahoo.com>
2022-01-23 23:29:02 +0330

    added an event listener for new task form

--- basics/projects/todo-list/script.js
+++ basics/projects/todo-list/script.js
@@ -7,6 +7,8 @@ const listTitleElement     = document.querySelector('[data-list-title]')
 const listCountElement     = document.querySelector('[data-list-count]')
 const tasksContainer       = document.querySelector('[data-tasks]')
 const taskTemplate         = document.getElementById('task-template')
+const newTaskForm          = document.querySelector('[data-new-task-form]')
+const newTaskInput         = document.querySelector('[data-new-task-input]')
 
 // store the user's data in the browser (local storage)
 // making a namespace prevents you from overwriting the info that is already in the storage, or preventing other websites from overwriting this local storage
@@ -18,11 +20,27 @@ let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 newListForm.addEventListener('submit', e => {
     // stops the page from submitting 
     e.preventDefault()
+
     const listName = newListInput.value
     if (listName == null || listName === '') return
+    
     const list = createList(listName)
     newListInput.value = null
     lists.push(list)
+    saveAndRender()
+})
+
+newTaskForm.addEventListener('submit', e => {
+    // stops the page from submitting 
+    e.preventDefault()
+
+    const taskName = newTaskInput.value
+    if (taskName == null || taskName === '') return
+    
+    const task = createTask(taskName)
+    newTaskInput.value = null
+    const selectedList = lists.find(list => list.id === selectedListId)
+    selectedList.tasks.push(task) // same as lists.push(list)
     saveAndRender()
 })
 
@@ -41,11 +59,11 @@ deleteListButton.addEventListener('click', e => {
 })
 
 function createList(name) {
-    return { id: Date.now().toString(), name: name, tasks: [{
-        id: 1,
-        name: 'wefef',
-        complete: false
-    }] }
+    return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function createTask(name) {
+    return { id: Date.now().toString(), name: name, complete: false }
 }
 
 function saveAndRender() {
@@ -60,7 +78,7 @@ function save() {
 
 function render() {
     clearElement(listsContainer)
-    renderList()
+    renderLists()
 
     const selectedList = lists.find(list => list.id === selectedListId)
     if (selectedListId == null) {
@@ -74,15 +92,17 @@ function render() {
     }
 }
 
-function renderList() {
+function renderLists() {
     lists.forEach(list => {
         const listElement = document.createElement('li')
         listElement.classList.add("list-name")
         listElement.dataset.listId = list.id
         listElement.innerText = list.name
+
         if (list.id === selectedListId) {
             listElement.classList.add('active-list')
         }
+
         listsContainer.appendChild(listElement)
     })
 }
@@ -90,14 +110,16 @@ function renderList() {
 function renderTasks(selectedList) {
     selectedList.tasks.forEach(task => {
         // use a template 
-        const taskElement = document.importNode(
-            taskTemplate.content, true)
+        const taskElement = document.importNode(taskTemplate.content, true)
         const checkbox = taskElement.querySelector('input')
+        
         checkbox.id = task.id
         checkbox.checked = task.complete
+        
         const label = taskElement.querySelector('label')
-        label.htmlFor = task.id // ???
+        label.htmlFor = task.id 
         label.append(task.name)
+
         tasksContainer.appendChild(taskElement)
     })
 }
