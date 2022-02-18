36733d9d46c4365b6e76df9c9c3b366ddb67338f
Kimia Montazeri <kimia.montazeri@yahoo.com>
2022-02-18 18:15:58 +0330

    Add createCard method

--- react.js/contacts-page/src/App.js
+++ react.js/contacts-page/src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import Card from './components/Card'
 import contacts from './contacts.js'
 
+
+function createCard(contact) {
+  return <Card
+    name= {contact.name}
+    imgURL= {contact.imgURL}
+    phone= {contact.phone}
+    email= {contact.email}
+  />
+}
+
 function App() {
   return (
     <div>
       <h1 className='heading'>My Contacts</h1>
-      <Card name={contacts[0].name} imgURL={contacts[0].imgURL} phone={contacts[0].phone} email={contacts[0].email}/>
-      <Card name={contacts[1].name} imgURL={contacts[1].imgURL} phone={contacts[1].phone} email={contacts[1].email}/>
-      <Card name={contacts[2].name} imgURL={contacts[2].imgURL} phone={contacts[2].phone} email={contacts[2].email}/>
+      {contacts.map(createCard)}
     </div>
   );
 }
